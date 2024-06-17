e9240d446fe26969b74b0ce3e2490dea00821017
Dylan Abbett <abbettdylan@gmail.com>
2024-06-17 13:26:06 -0400

    Initial commit

---
+++ src/app/components/enter-budget/enter-budget.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enter-budget',
+  standalone: true,
+  imports: [],
+  templateUrl: './enter-budget.component.html',
+  styleUrl: './enter-budget.component.scss'
+})
+export class EnterBudgetComponent {
+
+}
